a658b2898210067b9af2ed1e0cb7ce38f5c4092d
yukten <yuk7829@gmail.com>
2026-03-15 17:08:47 +0900

    パスワード設定メール

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get 'service-worker' => 'rails/pwa#service_worker', as: :pwa_service_worker
   get 'manifest' => 'rails/pwa#manifest', as: :pwa_manifest
 
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   devise_for :users
 
   # Defines the root path route ("/")
