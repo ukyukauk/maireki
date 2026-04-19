ec8d3c47f75bcf95e8c79c4a1d4cce568fde3aa5
yukten <yuk7829@gmail.com>
2026-04-19 16:33:07 +0900

    shrine_spec

--- spec/models/visit_spec.rb
+++ spec/models/visit_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Visit, type: :model do
-  let!(:user) { create(:user) }
+  let(:user) { create(:user) }
 
   context '参拝日と神社が入力されており、参拝日が今日以前の場合' do
-    let!(:shrine) { create(:shrine, user: user) }
-    let!(:visit) { build(:visit, user: user, shrine_id: shrine.id) }
+    let(:shrine) { create(:shrine, user: user) }
+    let(:visit) { build(:visit, user: user, shrine: shrine) }
 
     it '参拝記録が保存できる' do
       expect(visit).to be_valid
@@ -13,8 +13,8 @@ RSpec.describe Visit, type: :model do
   end
 
   context '参拝日が未入力の場合' do
-    let!(:shrine) { create(:shrine, user: user) }
-    let!(:visit) { build(:visit, user: user, shrine_id: shrine.id, visited_on: nil) }
+    let(:shrine) { create(:shrine, user: user) }
+    let(:visit) { build(:visit, user: user, shrine: shrine, visited_on: nil) }
 
     it '参拝記録が保存できない' do
       expect(visit).to be_invalid
@@ -23,7 +23,7 @@ RSpec.describe Visit, type: :model do
   end
 
   context '神社が未入力の場合' do
-    let!(:visit) { build(:visit, user: user, shrine_id: nil) }
+    let(:visit) { build(:visit, user: user, shrine_id: nil) }
 
     it '参拝記録が保存できない' do
       expect(visit).to be_invalid
@@ -32,8 +32,8 @@ RSpec.describe Visit, type: :model do
   end
 
   context '参拝日が未来日の場合' do
-    let!(:shrine) { create(:shrine, user: user) }
-    let!(:visit) { build(:visit, user: user, shrine_id: shrine.id, visited_on: Date.tomorrow) }
+    let(:shrine) { create(:shrine, user: user) }
+    let(:visit) { build(:visit, user: user, shrine: shrine, visited_on: Date.tomorrow) }
 
     it '参拝記録が保存できない' do
       expect(visit).to be_invalid
@@ -41,5 +41,4 @@ RSpec.describe Visit, type: :model do
       expect(visit.errors[:visited_on]).to include("は未来の日付にできません")
     end
   end
-
 end
